86a82187f1385ad2a352b8927609e8b108657608
Zhizhik <zhizhik@iMac-Zhizhik.local>
2014-12-28 02:25:33 -0800

    Initial Commit

---
+++ homeWork6TableWithModels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  homeWork6TableWithModels
+//
+//  Created by Zhizhik on 28.12.14.
+//  Copyright (c) 2014 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ homeWork6TableWithModels/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homeWork6TableWithModels
+//
+//  Created by Zhizhik on 28.12.14.
+//  Copyright (c) 2014 c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
